0801e81d826dccb9d2bd9d0819d4c2a4dad1855f
Richa <richierich2801@gmail.com>
2022-06-03 17:34:22 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,26 @@
+document.querySelector("#login").addEventListener('click', validate);
+
+
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Richa" && password == "1234"){
+alert ("Login successfully");
+window.location = "main.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
